ecf070e91d16de5c2fae297d8902f546e8f69771
Robert Grizzell <robert@grizzell.me>
2024-07-28 11:53:47 -0500

    Adding support for LILYGO T-Keyboard

---
+++ ports/espressif/boards/lilygo_tkeyboard/mpconfigboard.h
@@ -0,0 +1,13 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2024 Adafruit Industries LLC
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#define MICROPY_HW_BOARD_NAME       "LILYGO T-KEYBOARD"
+#define MICROPY_HW_MCU_NAME         "ESP32-C3"
+
+#define DEFAULT_SPI_BUS_SCK         (&pin_GPIO20)
+#define DEFAULT_SPI_BUS_MOSI        (&pin_GPIO10)
